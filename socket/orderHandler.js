17b5167f64f7d21ef3a776b89bc90a2b3d37f175
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2026-03-26 01:56:14 +0600

    Update place order of orderHandler

--- socket/orderHandler.js
+++ socket/orderHandler.js
@@ -1,4 +1,5 @@
-import { validateOrder } from "../utils/helper.js";
+import { getCollection } from "../config/database.js";
+import { calculateTotals, createOrderDocument, generateOrderId, validateOrder } from "../utils/helper.js";
 
 export const orderHandler = (io, socket) => {
     console.log("working smooth!!!!!!!!!!!!!", socket.id);
@@ -11,8 +12,24 @@ export const orderHandler = (io, socket) => {
             console.log(`placed order from ${socket.id}`);
             const validation = validateOrder(data);
             if(!validation.valid) return Callback({ success: false, message: validation.message })
+            
+            const totals = calculateTotals(data.items);
+            const orderId = generateOrderId();
+            const order = createOrderDocument(data, orderId, totals);
+
+            const ordersCollection = getCollection('orders');
+            await ordersCollection.insertOne(order);
+
+            socket.join(`order-${orderId}`);
+            socket.join('customers');
+
+            io.to('admins').emit('newOrder', { order })
+
+            Callback({ success: true, order });
+            console.log(`order created ${orderId}`);
         } catch (error) {
             console.log(error);
+            Callback({ success: false, message: 'Failed to place order' })
         }
     })
 
