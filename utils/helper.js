1e6eaa9acac5aec6beabbc7b4b38f5c681fc8eae
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2026-03-29 23:32:39 +0600

    Update the generateOrderId function

--- utils/helper.js
+++ utils/helper.js
@@ -15,7 +15,7 @@ export function generateOrderId() {
     const now = new Date();
     const year = now.getFullYear();
     const month = String(now.getMonth() + 1).padStart(2, '0');
-    const day = String(now.getDay()).padStart(2, '0');
+    const day = String(now.getDate()).padStart(2, '0');
     const random = Math.floor(Math.random()*1000).toString().padStart(3, '0');
     
     return `ORD-${year}${month}${day}-${random}`;
